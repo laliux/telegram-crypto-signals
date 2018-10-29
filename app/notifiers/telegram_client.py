31d57da94d784d421027e31fb0a027a96fba0773
Eduardo Zarate <eduardo@bbfour.com>
2018-10-29 17:40:44 -0600

    Added several commands to the Telegram Bot

--- app/notifiers/telegram_client.py
+++ app/notifiers/telegram_client.py
@@ -26,11 +26,11 @@ class TelegramNotifier(NotifierUtils):
         self.logger = structlog.get_logger()
         #self.bot = telegram.Bot(token=token)
         # Create the EventHandler and pass it your bot's token.
-        self.updater = Updater(token)
+        #self.updater = Updater(token)
         self.chat_id = chat_id
         self.parse_mode = parse_mode
 
-        self.setup_bot()
+        #self.setup_bot()
 
     @retry(
         retry=retry_if_exception_type(telegram.error.TimedOut),
@@ -65,54 +65,9 @@ class TelegramNotifier(NotifierUtils):
 
         self.updater.bot.send_photo(chat_id=self.chat_id, photo=photo_url, caption=caption, timeout=40)
 
-    def register_markets(self, markets):
-        self.logger.info('entro a este metodo %s' , markets)
-
     def stop_bot(self):
         if self.updater.running :
             self.updater.stop()
 
-    def setup_bot(self):
-        """Start the bot."""
-        # Get the dispatcher to register handlers
-        dp = self.updater.dispatcher
-
-        # on different commands - answer in Telegram
-        dp.add_handler(CommandHandler("start", start))
-        dp.add_handler(CommandHandler("help", help))
-
-        # on noncommand i.e message - echo the message on Telegram
-        dp.add_handler(MessageHandler(Filters.text, echo))
-
-        # log all errors
-        dp.add_error_handler(error)
-
-        #updater.bot.send_message(chat_id=20739325, text='Holi')
-
-        # Start the Bot
-        self.updater.start_polling()
-
-        # Run the bot until you press Ctrl-C or the process receives SIGINT,
-        # SIGTERM or SIGABRT. This should be used most of the time, since
-        # start_polling() is non-blocking and will stop the bot gracefully.
-        #self.updater.idle()     
-
-
-def start(bot, update):
-        """Send a message when the command /start is issued."""
-        update.message.reply_text('Hi!')
-
-
-def help(bot, update):
-        """Send a message when the command /help is issued."""
-        update.message.reply_text('TODO: show help')
-
-
-def echo(bot, update):
-        """Echo the user message."""
-        reply = "Unknown command " + update.message.text
-        update.message.reply_text(reply)
-
-def error(bot, update, error):
-        """Log Errors caused by Updates."""
-        #logger.warning('Update "%s" caused error "%s"', update, error)           
+    def setup_bot(self, updater):
+        self.updater = updater       
